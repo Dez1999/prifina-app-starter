535a8f1dfa98f98c29bdb8bcd07e19fc5ec8f41c
Hamza Halilovic <hamzahalilovic.12@gmail.com>
2021-03-06 11:10:34 +0100

    Frontend build (#4)
    * testing build
    
    * added sticky navbar && replace material with chakra
    
    * next stage
    
    * fix
    
    * FIX
    
    Co-authored-by: hamza <hamza.halilovic@mop.ba>

--- src/components/ProgressCard.js
+++ src/components/ProgressCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 
 import { Text, Flex } from "@blend-ui/core";
 
@@ -11,21 +10,11 @@ import {
   CircularProgress,
   CircularProgressLabel,
 } from "@chakra-ui/react";
-=======
-import { Link } from "react-router-dom";
-
-import { ThemeProvider, Text, Avatar, Flex } from "@blend-ui/core";
-import { BlendIcon } from "@blend-ui/icons";
-import Box from "@blend-ui/core/dist/esm/Box";
-
-import CircularProgress from "@material-ui/core/CircularProgress";
->>>>>>> d1769936e657d3c122d68a687780bd0fcdc59a32
 
 const styles = {
   boxShadow: " 0px 5px 20px #F0F4F8",
 };
 
-<<<<<<< HEAD
 const config = {
   initialColorMode: "light",
   useSystemColorMode: false,
@@ -64,37 +53,6 @@ function ProgressCard({ title, subtitle, color, value }) {
         </Box>
       </Flex>
     </ThemeProvider>
-=======
-function ProgressCard({ title, subtitle, color, value }) {
-  return (
-    <Flex
-      flex={1}
-      width="353px"
-      height="137px"
-      borderRadius={10}
-      style={styles}
-      padding={20}
-    >
-      <CircularProgress
-        variant="determinate"
-        value={value}
-        style={{ width: 95, height: 95 }}
-        color={color}
-      />
-      <Box marginLeft={35}>
-        <Box marginBottom={5} marginTop={27}>
-          <Text color="#5F6AC4" fontSize={28}>
-            {title}
-          </Text>
-        </Box>
-        <Box>
-          <Text color="#5F6AC4" fontSize={18}>
-            {subtitle}
-          </Text>
-        </Box>
-      </Box>
-    </Flex>
->>>>>>> d1769936e657d3c122d68a687780bd0fcdc59a32
   );
 }
 
